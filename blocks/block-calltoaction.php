e1c09cb8bd77b02eb9fef03138c3dda726f1c1fd
Leeland Clenista <leeland_clenista@yahoo.com>
2019-05-07 21:52:50 -0700

    added styling to call-to-action block

--- blocks/block-calltoaction.php
+++ blocks/block-calltoaction.php
@@ -1,5 +1,9 @@
-<h4><?php block_field( 'title'); ?></h4>
-<p><?php block_field( 'text'); ?></p>
-<a href="<?php block_field( 'button-url'); ?>">
-  <?php block_field( 'button-text') ; ?>
-</a>
+<?php echo '<link rel="stylesheet" type="text/css" href="style.css" media="screen" />'; ?>
+
+<div class="container">
+  <h4><?php block_field( 'title'); ?></h4>
+  <p><?php block_field( 'text'); ?></p>
+  <a class="call-button" href="<?php block_field( 'button-url'); ?>">
+    <?php block_field( 'button-text') ; ?>
+  </a>
+</div>
